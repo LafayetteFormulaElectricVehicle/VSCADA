2253876ff8ca5b2a42cce77ebba3c481e3cb13bf
wilesa <austinwiles95@gmail.com>
2017-02-27 10:54:34 -0500

    working on server

--- webserver/src/main/java/main.java
+++ webserver/src/main/java/main.java
@@ -6,11 +6,17 @@
 
 import static spark.Spark.*;
 import com.google.gson.*;
+import spark.Request;
+
+import javax.servlet.MultipartConfigElement;
+import java.io.InputStream;
 
 public class main {
 
     public static void main(String[] args) {
         port(3000);
+
+        staticFiles.location("/");
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
@@ -18,7 +24,9 @@ public class main {
         System.out.println(gson.toJson(packet));
 
         get("/",(req,res) -> {
-            res.redirect("/data");
+            res.redirect("/app/home.html");
+            res.header("Access-Control-Allow-Origin","*");
+            System.out.println(req.raw());
             return null;
         });
 
@@ -28,6 +36,41 @@ public class main {
             return gson.toJson(packet);
         });
 
+        get("/api/name?name", (req, res) -> {
+            printReq(req);
+            req.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+            try (InputStream is = req.raw().getPart("uploaded_file").getInputStream()) {
+                // Use the input stream to create a file
+            }
+            System.out.println(req.attributes());
+            return null;
+        });
+
+        notFound((req,res) -> {
+            System.out.println("NOT FOUND");
+            printReq(req);
+            return "Not found";
+        });
+
+    }
+
+    private static void printReq(Request req) {
+        System.out.println(req.url());
+        System.out.println(req.attributes());
+        System.out.println(req.params());
+        System.out.println(req.headers());
+        System.out.println(req.requestMethod());
+        System.out.println(req.scheme());
+        System.out.println(req.body());
+//        System.out.println();
+//        System.out.println();
+//        System.out.println();
+//        System.out.println();
+//        System.out.println();
+//        System.out.println();
+//        System.out.println();
+//        System.out.println();
+//        System.out.println();
     }
 
 }
